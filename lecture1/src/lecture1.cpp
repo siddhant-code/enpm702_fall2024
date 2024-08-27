cf2be7b0b6f6c9e03a8023a7616ce6fc8e6c82b5
zeidk <zeidk@umd.edu>
2024-08-27 18:34:02 -0400

    edited lecture1.cpp

--- lecture1/src/lecture1.cpp
+++ lecture1/src/lecture1.cpp
@@ -1,5 +1,20 @@
+/**
+ * @file lecture1.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <iostream>
 
+/**
+ * @brief This is a MAIN function
+ * 
+ * @return int 0 means code exited properly
+ */
 int main() {
-    std::cout << "Hello world\n";
+    std::cout << "Hello worlds\n";
 }
