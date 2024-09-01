b5219877565cb271e66410678112630cc9f5c962
zeid <zeid.kootbally@nist.gov>
2024-09-01 19:20:32 -0400

    Getting ready for lecture 2

--- lecture2/src/lecture2.cpp
+++ lecture2/src/lecture2.cpp
@@ -1,11 +1,11 @@
+#include <iomanip>  // for output manipulator std::setprecision()
 #include <iostream>
-#include <iomanip> // for output manipulator std::setprecision()
+#include <limits>    // for the min and max range of a type
+#include <typeinfo>  // needed for typeid
 
-int main()
-{
-    // ################//
-    //--- slide 12 ---//
-    // ################//
+int main() {
+    //</> 1
+    //=====================
     //  int break1;  // OK
     //  int break_1; // OK
     //  int Break1;  // OK
@@ -13,71 +13,125 @@ int main()
     //  int _break1; // OK
     //  // int 1Break;  // Error: expected unqualified-id before numeric constant
 
-    // ################//
-    //--- slide 16 ---//
-    // ################//
+    //</> 2
+    //=====================
     //  int number = 20;
     //  std::cout << sizeof(number) << '\n'; // 4 bytes on my machine
     //  std::cout << sizeof(int) << '\n';    // 4 bytes on my machine
 
-    // ################//
-    //--- slide 22 ---//
-    // ################//
+    //</> 3
+    //=====================
     //  int number;                  // declaration
     //  number = 1;                  // assignment
     //  std::cout << number << '\n'; // 1
     //  number = 2;                  // assignment
     //  std::cout << number << '\n'; // 2
 
-    // ################//
-    //--- slide 26 ---//
-    // ################//
+    //</> 4
+    //=====================
     // int a{};                                                      // initialized to 0
     // std::cout << a << '\n';                                       // 0
     // double b{};                                                   // initialized to 0.0
     // std::cout << b << '\n';                                       // 0
     // std::cout << std::fixed << std::setprecision(1) << b << '\n'; // 0.0
 
-    // ################//
-    //--- slide 27 ---//
-    // ################//
+    //</> 5
+    //=====================
     // int a{};   // the value of a will be replaced later
     // int b{0};  // we plan to use the value of b
     // a = b + 3; // value of b is used and a is assigned a new value
 
-    // ################//
-    //--- slide 28 ---//
-    // ################//
+    //</> 6
+    //=====================
     // int number;                  // uninitialized
     // std::cout << number << '\n'; // garbage
 
-    // ################//
-    //--- slide 36-1 ---//
-    // ################//
+    //</> 7
+    //=====================
+    // std::cout << "Type\t\tSize (bytes)\tMin Value\t\tMax Value\n";
+    // std::cout << "----------------------------------------------------------\n";
+
+    // std::cout << "char\t\t" << sizeof(char)
+    //           << "\t\t" << int(std::numeric_limits<char>::min())
+    //           << "\t\t\t" << int(std::numeric_limits<char>::max()) << '\n';
+
+    // std::cout << "unsigned char\t" << sizeof(unsigned char)
+    //           << "\t\t" << int(std::numeric_limits<unsigned char>::min())
+    //           << "\t\t\t" << int(std::numeric_limits<unsigned char>::max()) << '\n';
+
+    // std::cout << "int\t\t" << sizeof(int)
+    //           << "\t\t" << std::numeric_limits<int>::min()
+    //           << "\t\t" << std::numeric_limits<int>::max() << '\n';
+
+    // std::cout << "unsigned int\t" << sizeof(unsigned int)
+    //           << "\t\t" << std::numeric_limits<unsigned int>::min()
+    //           << "\t\t" << std::numeric_limits<unsigned int>::max() << '\n';
+
+    // std::cout << "short\t\t" << sizeof(short)
+    //           << "\t\t" << std::numeric_limits<short>::min()
+    //           << "\t\t" << std::numeric_limits<short>::max() << '\n';
+
+    // std::cout << "unsigned short\t" << sizeof(unsigned short)
+    //           << "\t\t" << std::numeric_limits<unsigned short>::min()
+    //           << "\t\t" << std::numeric_limits<unsigned short>::max() << '\n';
+
+    // std::cout << "long\t\t" << sizeof(long)
+    //           << "\t\t" << std::numeric_limits<long>::min()
+    //           << "\t\t" << std::numeric_limits<long>::max() << '\n';
+
+    // std::cout << "unsigned long\t" << sizeof(unsigned long)
+    //           << "\t\t" << std::numeric_limits<unsigned long>::min()
+    //           << "\t\t" << std::numeric_limits<unsigned long>::max() << '\n';
+
+    // std::cout << "long long\t" << sizeof(long long)
+    //           << "\t\t" << std::numeric_limits<long long>::min()
+    //           << "\t\t" << std::numeric_limits<long long>::max() << '\n';
+
+    // std::cout << "unsigned long long\t" << sizeof(unsigned long long)
+    //           << "\t\t" << std::numeric_limits<unsigned long long>::min()
+    //           << "\t\t" << std::numeric_limits<unsigned long long>::max() << '\n';
+
+    //</> 8-1
+    //=====================
+    // std::cout << std::fixed << std::setprecision(1);
+    // std::cout << "Type\t\tSize (bytes)\tMin Value\t\t\tMax Value\n";
+    // std::cout << "---------------------------------------------------------------\n";
+
+    // std::cout << "float\t\t" << sizeof(float)
+    //           << "\t\t" << std::numeric_limits<float>::min()
+    //           << "\t\t" << std::numeric_limits<float>::max() << '\n';
+
+    // std::cout << "\ndouble\t\t" << sizeof(double)
+    //           << "\t\t" << std::numeric_limits<double>::min()
+    //           << "\t\t" << std::numeric_limits<double>::max() << '\n';
+
+    // std::cout << "\nlong double\t" << sizeof(long double)
+    //           << "\t\t" << std::numeric_limits<long double>::min()
+    //           << "\t\t" << std::numeric_limits<long double>::max() << '\n';
+
+    //</> 8-2
+    //=====================
     // std::cout << 1.05 << '\n';  // this is a double
     // std::cout << 1.05f << '\n'; // this is a float
     // std::cout << 1f << '\n';    // error
     // std::cout << 1.0f << '\n';  // OK
 
-    // ################//
-    //--- slide 36-2 ---//
-    // ################//
+    //</> 8-3
+    //=====================
     // std::cout << std::setprecision(9);             // show 9 digits of precision
     // std::cout << 0.33333333333f << '\n';           // 0.333333343
     // std::cout << std::setprecision(15) << '\n';    // show 15 digits of precision
     // std::cout << 8.3642343534322323232322 << '\n'; // 8.36423435343223 (15 digits)
 
-    // ################//
-    //--- slide 37-1 ---//
-    // ################//
+    //</> 9-1
+    //=====================
     // bool is_today_sunny{true};
     // bool is_today_cloudy{false};
     // std::cout << is_today_sunny << '\n';  // 1
     // std::cout << is_today_cloudy << '\n'; // 0
 
-    // ################//
-    //--- slide 37-2 ---//
-    // ################//
+    //</> 9-2
+    //=====================
     // bool is_today_sunny{true};
     // bool is_today_cloudy{false};
     // std::cout << std::boolalpha << is_today_sunny << '\n';  // true
@@ -87,11 +141,54 @@ int main()
     // std::cout << std::boolalpha << false << '\n';           // false
     // std::cout << std::noboolalpha << false << '\n';         // 0
 
-    // ################//
-    //--- slide 39 ---//
-    // ################//
+    //</> 10
+    //=====================
     // int a{1};         // initialize variable a with literal value 1
     // int b{2 + 3 - 1}; // initialize variable b with computed value 4
     // int c{2 * 2 + 1}; // initialize variable c with computed value 5
     // int d{b};         // initialize variable d with variable value 4
+
+    //</> 11
+    //=====================
+    // double num1 = 1.5;
+    // int num2 = num1;                                                // 1.5 converted to 1
+    // std::cout << "Value of num1 : " << num1 << '\n';                // 1.5
+    // std::cout << "Type of num1 : " << typeid(num1).name() << '\n';  // double
+    // std::cout << "Value of num2 : " << num2 << '\n';
+
+    //</> 12-1
+    //=====================
+    // double num1{5.0};   // no promotion necessary
+    // double num2{4.0f};  // float promoted to double
+
+    //</> 12-2
+    //=====================
+    // short s = 1;
+    // int num1 = s;                    // short promoted to int
+    // int num2 = 'a';                  // char promoted to int
+    // int num3 = true;                 // bool promoted to int
+    // std::cout << num1 << '\n';       // 1
+    // std::cout << num2 << '\n';       // 97
+    // std::cout << num3 << '\n';       // 1
+    // std::cout << sizeof(s) << '\n';  // 2
+
+    //</> 13
+    //=====================
+    // int a;
+    // int b = 3.2;
+    // int c(1.3);
+    // int d{3.5};
+    // std::cout << a << '\n';
+    // std::cout << b << '\n';
+    // std::cout << c << '\n';
+    // std::cout << d << '\n';
+
+    //</> 14
+    //=====================
+    // int b = static_cast<int> (3.2);
+    // std::cout << b << '\n';
+    // int c(static_cast<int> (1.3));
+    // std::cout << c << '\n';
+    // int d{static_cast<int> (3.5)};
+    // std::cout << d << '\n';
 }
