2adc1ba58585fac98ea9f1fd7a8335c18340c695
zeid <zeid.kootbally@nist.gov>
2024-09-02 16:19:33 -0400

    Added slide code

--- lecture2/src/lecture2.cpp
+++ lecture2/src/lecture2.cpp
@@ -1,7 +1,56 @@
+#include "lecture2.hpp"
+
 #include <iomanip>  // for output manipulator std::setprecision()
 #include <iostream>
 #include <limits>    // for the min and max range of a type
 #include <typeinfo>  // needed for typeid
+#define PI 3.14159   // symbolic constant
+#define SQUARE(x) ((x) * (x))
+
+//</> 31
+//=====================
+// int global_var{1};
+
+// void my_function() {
+//     global_var++;
+// }
+
+//</> 32
+//=====================
+// int global_x;
+// int global_y{1};
+
+//</> 33, 34, 35
+//=====================
+// // namespace MyNamespace
+// namespace MyNamespace {
+// int x{3};
+// int y{4};
+// }  // end namespace MyNamespace
+
+//</> 34
+//=====================
+// using namespace MyNamespace;
+
+//</> 35
+//=====================
+// using MyNamespace::x;
+
+//</> 36
+//=====================
+// // custom namespace
+// namespace MyNamespace {
+// int cout{1};
+// }  // end namespace MyNamespace
+// using namespace std;
+// using namespace MyNamespace;
+
+//</> 37
+//=====================
+// Create custom types using 'using'
+using Integer = int;
+using Float = float;
+using uint = unsigned int;
 
 int main() {
     //</> 1
@@ -191,4 +240,222 @@ int main() {
     // std::cout << c << '\n';
     // int d{static_cast<int> (3.5)};
     // std::cout << d << '\n';
+
+    //</> 15
+    //=====================
+    // int i = 42;
+    // double d = 3.14;
+
+    // std::cout << "Type of result: " << typeid(i + d).name() << '\n';  // double
+    // std::cout << "Value of result: " << i + d << '\n';                // 45.14
+
+    // unsigned int ui = 100;
+    // long l = 5000;
+
+    // std::cout << "Type of result: " << typeid(ui + l).name() << '\n';  // long
+    // std::cout << "Value of result: " << ui + l << '\n';                // 5100
+
+    // unsigned short us = 10;
+    // unsigned long ul = 700000;
+
+    // std::cout << "Type of result: " << typeid(us + ul).name() << '\n';  // unsigned long
+    // std::cout << "Value of result: " << us + ul << '\n';                // 700010
+
+    //</> 16
+    //=====================
+    // short s1 = 100;
+    // char c = 50;
+    // std::cout << "Type of result: " << typeid(s1 + c).name() << '\n'; // int
+    // std::cout << "Value of result: " << s1 + c << '\n'; // 150
+
+    // unsigned char uc = 200;
+    // bool b1 = true;
+    // std::cout << "Type of result: " << typeid(uc + b1).name() << '\n'; // int
+    // std::cout << "Value of result: " << uc + b1 << '\n'; // 201
+
+    // bool b2 = false;
+    // short s2 = 32767;
+    // std::cout << "Type of result: " << typeid(b2 + s2).name() << '\n'; // int
+    // std::cout << "Value of result: " << b2 + s2 << '\n'; // 32767
+
+    //</> 17
+    //=====================
+    // int a{3};
+    // int b{2};
+    // std::cout << "Type of result: " << typeid(a / b).name() << '\n';
+    // std::cout << "Value of result: " << a / b << '\n';
+
+    //</> 19
+    //=====================
+    // int x{1};
+    // // Output the value of the variable 'x'
+    // std::cout << x << '\n';
+    // // Output multiple strings in a single statement
+    // std::cout << "hello, " << "world\n";
+    // // Output a combination of a variable and a literal of different data types
+    // std::cout << x << " x " << 3.4 << '\n';
+
+    //</> 20-1
+    //=====================
+    // // ask user for their age
+    // std::cout << "Enter your age: ";
+    // // init variable to hold user input
+    // unsigned short age{};
+    // // store input in variable age
+    // std::cin >> age;
+    // std::cout << "Your age is " << age << '\n';
+
+    //</> 20-2
+    //=====================
+    // std::cout << "Enter two integers separated by a space: ";
+    // // init variable to hold the first number
+    // int a{};
+    // // init variable to hold the second number
+    // int b{};
+    // // store inputs in variables a and b
+    // std::cin >> a >> b;
+    // std::cout << "a: " << a << ", b: " << b << '\n';
+
+    //</> 21
+    //=====================
+    // std::cout << "Enter two integers separated by a space: ";
+    // // init variable to hold the first number
+    // int a{};
+    // // init variable to hold the second number
+    // double b{};
+    // // store inputs in variables a and b
+    // std::cin >> a >> b;
+    // std::cout << "a: " << a << ", b: " << b << '\n';
+
+    //</> 22-1
+    //=====================
+    // const double pi;
+
+    //</> 22-2
+    //=====================
+    // const double pi{3.141598};
+    // pi = 3.14;
+
+    //</> 23
+    //=====================
+    // std::cout << "pi: " << PI << '\n';  // preprocessor replaces PI with 3.14159
+
+    //</> 24-1
+    //=====================
+    // int a = 5;
+    // double result = SQUARE(a);
+    // std::cout << result << '\n';
+
+    //</> 24-2
+    //=====================
+    // double area = PI * 10 * 10;
+    // std::cout << area << '\n';
+
+    //</> 25-1
+    //=====================
+    // const int a{1};              // a is a compile-time const
+    // const int b{2};              // b is a compile-time const
+    // std::cout << a + b << '\n';  // a + b is a compile-time expression
+
+    //</> 25-2
+    //=====================
+    // std::cout << "Enter an integer: ";
+    // int input{};
+    // std::cin >> input;
+    // const int a{1};              // a is a compile-time const
+    // const int b{input};          // b is a run-time const
+    // std::cout << a + b << '\n';  // a + b is a run-time expression
+
+    //</> 26
+    //=====================
+    // constexpr int a{1};  // OK: a is a compile-time const
+    // constexpr int b{2};  // OK: b is a compile-time const
+    // std::cout << "Enter an integer: ";
+    // int input{};
+    // std::cin >> input;
+    // constexpr int c{input};  // error
+
+    //</> 27
+    //=====================
+    // auto a{3.0};    // 3.0 is a double literal, so variable a will be type double
+    // std::cout << "Type of a: " << typeid(a).name() << '\n'; // double
+    // auto b{1 + 2};  // 1 + 2 evaluates to an int, so b will be type int
+    // std::cout << "Type of b: " << typeid(b).name() << '\n';  // int
+    // auto c{b};      // variable b is an int, so c will be type int
+    // std::cout << "Type of c: " << typeid(c).name() << '\n';  // int
+
+    //</> 28-1
+    //=====================
+    // const int a{5};  // a is const
+    // auto b{a};       // b is int (const is dropped)
+    // b = 1;           // OK
+
+    //</> 28-2
+    //=====================
+    // constexpr int a{5};   // a is const int
+    // constexpr auto b{a};  // b is const int
+    // b = 1;                // error: assignment of read-only variable 'b'
+
+    //</> 29
+    //=====================
+    // int a{};
+    // {  // start nested block 1
+    //     int b{};
+    //     {  // start nested block 2
+    //         int c{};
+    //     }  // end nested block 2
+    // }  // end nested block 1
+
+    //</> 30
+    //=====================
+    // int a{1};
+    // {
+    //     int b{2};
+    //     std::cout << a << '\n';  // 1
+    //     std::cout << b << '\n';  // 2
+    // }  // b goes out of scope here.
+    // // std::cout << b << '\n';  // error: b is out of scope
+    // int c{3};
+
+    //</> 31
+    //=====================
+    // std::cout << global_var << '\n';  // 1
+    // global_var++;                     // 2
+    // my_function();                    // 3
+    // std::cout << global_var << '\n';  // 3
+
+    //</> 32
+    //=====================
+    // std::cout << &global_x << '\n';
+    // std::cout << &global_y << '\n';
+
+    //</> 33
+    //=====================
+    // std::cout << MyNamespace::x << '\n';  // 3
+    // std::cout << MyNamespace::y << '\n';  // 4
+
+    //</> 34
+    //=====================
+    // std::cout << x << '\n';  // no need to use MyNamespace::x
+    // std::cout << y << '\n';  // no need to use MyNamespace::y
+
+    //</> 35
+    //=====================
+    // std::cout << x << '\n';  // no need to use MyNamespace::x
+    // std::cout << y << '\n';  // error: ‘y’ was not declared in this scope
+
+    //</> 36
+    //=====================
+    // cout << cout << '\n';
+
+    //</> 37
+    //=====================
+    // Use the custom types
+    // Integer a{10};
+    // Float b{20.5f};
+    // uint age{30};
+
+    // std::cout << "Integer: " << a << '\n';
+    // std::cout << "Float: " << b << '\n';
+    // std::cout << "Age: " << age << '\n';
 }
